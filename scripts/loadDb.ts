b5c91f9570b7a3d5dd482b92fd83b3a2ba6e451a
abderrahmenyoussef <abderrahmen.youssef@polytechnicien.tn>
2025-07-22 03:24:23 +0100

    Add environment configuration and update .gitignore for sensitive files

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -3,3 +3,5 @@ import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer
 import OpenAI from "openai"
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
 import "dotenv/config"
+
+const { ASTRA_DB_NAMESPACE, ASTRA_DB_COLLECTION, ASTRA_DB_API_ENDPOINT, ASTRA_DB_APPLICATION_TOKEN, OPENAI_API_KEY } = process.env
